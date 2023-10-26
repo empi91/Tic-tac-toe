7693ece7d3bdb210040c7c9ed357e23efbd0ba87
empi91 <korpet.filip@gmail.com>
2023-10-26 23:21:14 +0200

    Starting menu done

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,6 @@
 import random
 
+finished = False
 board_data = [ [".", ".", "."], [".", ".", "."], [".", ".", "."]]
 
 def draw_board():
@@ -19,12 +20,28 @@ def draw_board():
             
         row_counter += 1
 
+def print_start_menu():
+    print("Hello! \nIn this game you are playing a tic-tac-toe game with computer. \nRules are simple: \n- game board is 3x3; \n- you can draw X or O in an empty field; \n- Whoever gets three X or O in the row (vertical, horizontal or diagonally) wins \nGood Luck!")
+
+def print_move_menu():
+    pass
+
+def print_finish_menu():
+    pass
+
 def move():
+    print_menu()
+    save_move_to_board()
+    draw_board()
     pass
 
 
-
 ##  __main__
-draw_board()
+print_start_menu()
+
+while not finished:
+    draw_board()
+    print_move_menu()
+
 
 
